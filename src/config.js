0e7e1f3562f283c963ae2c51f1965e7df48d4890
Brandon <25nguyenb26@stu.smuhsd.org>
2023-06-10 10:58:32 -0700

    Combining into config
    Just use one config file rather than splitting up test data into two seperate files

---
+++ src/config.js
@@ -0,0 +1,15 @@
+const config = {
+	// Endpoints
+	/** The endpoint for getting the court appearances of a person */
+	getAppearances: "https://api.sampleapis.com/jokes/goodJokes",
+
+	/** The endpoint for subscribing to notifications */
+	subNotifications: "https://api.sampleapis.com/jokes/goodJokes",
+
+	// dev-mode, allows for direct toggling between features
+	devMode: true,
+
+	// Mock data
+};
+
+export default config;
