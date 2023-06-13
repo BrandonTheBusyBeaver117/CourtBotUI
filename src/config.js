c21969858750de9b8d01420b61976c2ecc8848db
Brandon <25nguyenb26@stu.smuhsd.org>
2023-06-13 16:24:14 -0700

    Adding keys to config
    easier configuration later

--- src/config.js
+++ src/config.js
@@ -1,62 +1,15 @@
-const config = {
-	// Endpoints
-	/** The endpoint for getting the court appearances of a person */
-	getAppearances: "/getAppearances",
-
-	/** The endpoint for subscribing to notifications */
-	subNotifications: "/subNotifications",
-
-	// Developer tools:
-	// The following features can be helpful for fiddling with the website during dev work,
-	// but should usually be TURNED OFF when actually deploying the website
-
-	// Allows toggling between different possible layouts
-	toggleLayout: false,
-
-	maxNumberOfResults: 10,
-
-	/**
-	 * Setting 'useMockData' this on means that the website won't send requests, and
-	 * instead use the mock data defined below.
-	 */
-	useMockData: false,
-
-	// Not really used, but a list of random names
-	mockNames: ["John", "Adam", "Apple", "Joe", "Bob", "Zach", "Ted", "Mark", "Yee", "Richard", "Mall"],
-
-	/**
-	 * A mock array of 'appearance' object literals
-	 */
-	mockAppearances: [
-		{
-			uuid: "hfjkshdfksfdhj-yydsfjh-fgjhsdjgdfs",
-			datetime: "2023-07-15 12:30 PST",
-			parties: ["Bob Smith", "State of California"],
-			location: "Santa Clara County Main Courthouse",
-			department: "25J",
-		},
-		{
-			uuid: "6a8e9ca0-081d-11ee-be56-0242ac120002",
-			datetime: "2023-08-15 2:30 PST",
-			parties: ["Bob Smith", "State of California"],
-			location: "Santa Clara County Main Courthouse",
-			department: "27C",
-		},
-		{
-			uuid: "6a8e9f34-081d-11ee-be56-0242ac120002",
-			datetime: "2023-09-15 4:30 PST",
-			parties: ["Bob Smith", "State of California"],
-			location: "Santa Clara County Main Courthouse",
-			department: "29K",
-		},
-	],
-};
-
-export default config;
-
 const Endpoints = {
 	/** The endpoint for getting the court appearances of a person */
 	getAppearances: "/getAppearances",
+
+	/** The key in the getAppearances response containing the appearance data */
+	appearanceKey: "appearances",
+
+	/** The endpoint for getting all the peoples' names on file */
+	getAllNames: "/getAllNames",
+
+	/** The key in the getAllNames response containing the all the name data */
+	allNamesKey: "allNames",
 
 	/** The endpoint for subscribing to notifications */
 	subNotifications: "/subNotifications",
