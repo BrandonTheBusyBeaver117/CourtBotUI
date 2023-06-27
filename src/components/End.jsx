08d9c36ad8a38435928d83fc974c30a8d3009a7c
Brandon <25nguyenb26@stu.smuhsd.org>
2023-06-26 22:13:44 -0700

    Formatting the end screen

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -1,16 +1,17 @@
 import React, { useContext } from "react";
-import Back from "./Back";
-import Next from "./Next";
+import "./End.scss";
 import { Context } from "../State";
 
 const End = () => {
 	const [state, dispatch] = useContext(Context);
 
 	return (
-		<div classname="End">
-			<p>Success!</p>
-			<p>Your text reminders will begin shortly</p>
-			<p>Thanks for taking the time to use CourtBot!</p>
+		<div className="end">
+			<div className="text-end">
+				<p>Success!</p>
+				<p>Your text reminders will begin shortly</p>
+				<p>Thanks for taking the time to use CourtBot!</p>
+			</div>
 			<button onClick={() => dispatch({ type: "reset" })}>Get reminders for another person</button>
 		</div>
 	);
