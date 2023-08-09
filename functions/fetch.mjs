1e214de22a67f897d085e2d35b5fe6be0f0b7e1d
Brandon <25nguyenb26@stu.smuhsd.org>
2023-08-09 11:39:04 -0700

    maybe we just need to send data

--- functions/fetch.mjs
+++ functions/fetch.mjs
@@ -5,7 +5,7 @@ export const handler = async (event, context) => {
     // Cursed, but MVP lol
     const fetchedUrl = event.path.slice(event.path.indexOf("http")) 
 
-    const data = await axios.get(fetchedUrl).then(response => response)
+    const data = await axios.get(fetchedUrl).then(response => response.data)
 
     return JSON.stringify(data)
 };
