41d840c440fa0967b184147e542db6ab7e55647e
Brandon <25nguyenb26@stu.smuhsd.org>
2023-08-09 11:50:53 -0700

    a bit of logging

--- functions/fetch.mjs
+++ functions/fetch.mjs
@@ -3,9 +3,13 @@ import axios from "axios";
 export const handler = async (event, context) => {
 
     // Cursed, but MVP lol
+    // Slicing to get the actual url wanted
     const fetchedUrl = event.path.slice(event.path.indexOf("http")) 
 
     const data = await axios.get(fetchedUrl).then(response => response.data)
+
+    console.log(fetchedUrl)
+    console.log(data)
 
     return {
         statusCode: 200,
