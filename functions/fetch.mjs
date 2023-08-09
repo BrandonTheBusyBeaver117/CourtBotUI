1e606806ecf06578d42f375ffd079194c3912fe4
Brandon <25nguyenb26@stu.smuhsd.org>
2023-08-09 11:35:54 -0700

    final time?
    turns out you need to send a stringified response

--- functions/fetch.mjs
+++ functions/fetch.mjs
@@ -2,15 +2,10 @@ import axios from "axios";
 
 export const handler = async (event, context) => {
 
-    console.log(event)
-
     // Cursed, but MVP lol
     const fetchedUrl = event.path.slice(event.path.indexOf("http")) 
 
     const data = await axios.get(fetchedUrl).then(response => response)
 
-    console.log("end data")
-    console.log(data)
-
-    return data
+    return JSON.stringify(data)
 };
