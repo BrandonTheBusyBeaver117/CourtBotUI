a923b77b4a78240484a490bff364a9c567789750
Brandon <25nguyenb26@stu.smuhsd.org>
2023-08-09 10:50:12 -0700

    Getting serverless functions up
    Logging our function calls

--- src/Endpoints.js
+++ src/Endpoints.js
@@ -1,12 +1,12 @@
 const Endpoints = {
 
 	/** The endpoint for getting the court appearances of a person */
-	getAppearances: "http://50.116.13.181:8080/courtbot/getAppearances",
+	getAppearances: "api/http://50.116.13.181:8080/courtbot/getAppearances",
 
 	/** The endpoint for subscribing to notifications */
-	subNotifications: "http://50.116.13.181:8080/courtbot/register",
+	subNotifications: "api/http://50.116.13.181:8080/courtbot/register",
 
-	getRandom: "http://50.116.13.181:8080/courtbot/getRandom",
+	getRandom: "api/http://50.116.13.181:8080/courtbot/getRandom",
 };
 
 export default Endpoints;
