15af132dda92078c6c8f7513ca6d33b2dea1a05c
Brandon <25nguyenb26@stu.smuhsd.org>
2023-08-09 11:03:35 -0700

    simpler solution

--- src/Endpoints.js
+++ src/Endpoints.js
@@ -1,12 +1,14 @@
+const proxyUrl = ".netlify/functions/fetch/"
+
 const Endpoints = {
 
 	/** The endpoint for getting the court appearances of a person */
-	getAppearances: "api/http://50.116.13.181:8080/courtbot/getAppearances",
+	getAppearances: proxyUrl + "http://50.116.13.181:8080/courtbot/getAppearances",
 
 	/** The endpoint for subscribing to notifications */
-	subNotifications: "api/http://50.116.13.181:8080/courtbot/register",
+	subNotifications: proxyUrl + "http://50.116.13.181:8080/courtbot/register",
 
-	getRandom: "api/http://50.116.13.181:8080/courtbot/getRandom",
+	getRandom: proxyUrl + "http://50.116.13.181:8080/courtbot/getRandom",
 };
 
 export default Endpoints;
