2386b1e3cb1a09b6b2388297cdcd37f26d9e372b
Brandon <25nguyenb26@stu.smuhsd.org>
2023-08-09 10:58:11 -0700

    trying one more time

--- functions/fetch.mjs
+++ functions/fetch.mjs
@@ -4,7 +4,6 @@ export const handler = async (event, context) => {
 
     console.log(event)
     console.log(event.body)
-    console.log(JSON.stringify(event.body))
+    console.log(event.queryStringParameters)
     return axios.get("http://50.116.13.181:8080/courtbot/getRandom")
-   
 };
