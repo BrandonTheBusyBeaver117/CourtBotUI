9ef2b0d412395a826b6f40c83c170a5985b475d4
Brandon <25nguyenb26@stu.smuhsd.org>
2023-08-09 18:15:56 -0700

    Adding query params

--- functions/fetch.mjs
+++ functions/fetch.mjs
@@ -4,11 +4,27 @@ export const handler = async (event, context) => {
 
     // Cursed, but MVP lol
     // Slicing to get the actual url wanted
-    const fetchedUrl = event.path.slice(event.path.indexOf("http")) 
+    let fetchedUrl = event.path.slice(event.path.indexOf("http")) 
+
+    let firstIteration = true;
+
+    Object.entries(event.queryStringParameters).forEach((entry) => {
+
+        if(firstIteration){
+            fetchedUrl += "?"
+            firstIteration = false;
+        } else {
+            fetchedUrl += "&"
+        }
+
+        const [key, value] = entry
+        fetchedUrl += key + "=" + value
+    })
 
     const data = await axios.get(fetchedUrl).then(response => response.data)
 
-
+    console.log("Event:" + event)
+    console.log("raw url??" + event.url)
     console.log("raw path: " + event.path)
     console.log("query parameters path: " + JSON.stringify(event.queryStringParameters))
     console.log("fetchedurl: " + fetchedUrl)
